6809ef301245241b0f694d359d99ba3165c76cf6
karabo phahsa <johndoe@example.com>
2024-12-22 00:07:46 +0200

    testing understanding

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from db import users
 
 app = Flask(__name__)
 
 @app.route("/")
 def greet():
-    return("hello")
-
+    name = input("enter your fullname> ")
+    password = input('enter your password')
+    if (name, password) in users:
+        return "account already exist"
+    else:
+        users.append((name, password))
 
 if __name__ == "__main__":
     app.run(debug=True)
